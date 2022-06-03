9cb9592d98e1bf1572dd1fb67252db1574f01d96
OttaviaFeletig <ottafel@hotmail.it>
2022-06-03 11:00:12 +0200

    code review Ottavia

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
 
-
 function App() {
   return (
     <div>
-       <Profile />
-       <Home />
+      <Profile />
+      <Home />
       <Login />
       {/* <Register /> */}
-      
-
-
     </div>
-    
-  )
-  
-  
- 
+  );
 }
 
 export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,27 +1,28 @@
 import "./login.css";
-
+// ! Login and Registration should be the first pages you see, use React Router to achieve that
 export default function Login() {
-    return (
-        <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Gosocial</h3>
-                    <span className="loginDesc">
-                        Connect with friends around the world by sharing your most admirable tourists sights.
-                    </span>
-                </div>
-                <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <button className="loginButton">Log In</button>
-                        <span className="loginForgot">Forgot Password?</span>
-                        <button className="loginRegisterButton">
-                            Create a New Account
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Gosocial</h3>
+          <span className="loginDesc">
+            Connect with friends around the world by sharing your most admirable
+            tourists sights.
+          </span>
         </div>
-    );
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              Create a New Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
